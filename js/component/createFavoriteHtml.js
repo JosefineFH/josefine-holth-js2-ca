02989734c2002983382070c8a24db4e5052cba5a
Josefine Holth <Josefine.Holth@hotmail.com>
2022-03-11 12:28:20 +0100

    created create favorite html js file

---
+++ js/component/createFavoriteHtml.js
@@ -0,0 +1,43 @@
+import { removeFromFavorites } from "../util/removeFavorites.js";
+import { getFromStorage } from "../util/storage.js";
+
+let currentFavPosts;
+let container;
+
+viewFavorites();
+
+export function viewFavorites() {
+  currentFavPosts = getFromStorage("favorite_article");
+  container = document.querySelector(".post__container");
+
+  if (currentFavPosts.length === 0) {
+    console.log("There is noting her");
+    container.innerHTML = `<h3 style="line-height:2em">You don't have any posts stored just yet. So way dont you look <a href="/index.html">her</a> and find something.</h3>`;
+  }
+
+  currentFavPosts.forEach((post) => {
+    container.innerHTML += `
+    <div class="article__body">
+    <div class="article__img" style="background-image: url('${post.img}');">
+    <button class="remove_button" data-id="${post.id}">Remove as Favorite</button>
+    
+    </div>
+    <h2 class="article__title">${post.title}</h2>
+    
+    <a href="/singlePost.html?id=${post.id}">Read More</a>
+  </div>`;
+
+    const removeFavorite = document.querySelectorAll(".remove_button");
+    removeFavorite.forEach((button) => {
+      button.addEventListener("click", () => {
+        removePost(currentFavPosts);
+      });
+    });
+  });
+}
+
+function removePost(currentFavPosts) {
+  removeFromFavorites(currentFavPosts);
+  container.innerHTML = "";
+  viewFavorites();
+}
