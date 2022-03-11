086ad01934f684bbd43d7d0013f9ae65dc0c1ea3
Josefine Holth <Josefine.Holth@hotmail.com>
2022-03-11 12:25:42 +0100

    upated files

--- js/component/createHTML.js
+++ js/component/createHTML.js
@@ -1,34 +1,33 @@
-import { baseUrl } from "../data/api.js";
-import { saveToFavorites } from "../util/saveFavorite.js";
-import { getFromStorage } from "../util/storage.js";
+import { saveToFavorites } from "../utils/saveFavorite.js";
+import { getFromStorage } from "../utils/storage.js";
 
-export async function createFeaturedPosts() {
-  const futuresArticles = document.querySelector(".futures_articles");
-  const loader = document.querySelector(".fa-spin");
+export default function createFeaturedPosts(posts) {
+  const postContainer = document.querySelector(".post__container");
   let count = document.querySelector(".favorite_count");
   let currentFavPosts = getFromStorage("favorite_article");
 
   if (currentFavPosts.length === 0) {
-    count.innerHTML = `<p>You can add some articles if you want to read them later</p>`;
+    count.innerHTML = `<p >You can add some posts if you want to read them later</p>`;
   } else {
-    count.innerHTML = `<p>Articles in favorite: <span class="favorite_count">${currentFavPosts.length}</span></p>`;
+    count.innerHTML = `<p>Saved posts: <span class="favorite_count">${currentFavPosts.length}</span></p>`;
   }
   try {
-    const featuredPostUrl = baseUrl + "articles";
-    const response = await fetch(featuredPostUrl);
-    const posts = await response.json();
-    loader.style.display = "none";
-    
+    postContainer.innerHTML = "";
+
+    if (posts.length === 0) {
+      postContainer.innerHTML = `<p class="warning">There is no more posts her.</p>`;
+    }
+
     for (let i = 0; i < posts.length; i++) {
       const post = posts[i];
       let postId = post.id;
       const lastUpdated = post.updated_at;
       let postAuthor = post.author;
       const postTitle = post.title;
-      
+
       let imageURL;
       let altText;
-      
+
       if (post.cover[0] === undefined) {
         imageURL = "/assets/img/image_note_found.png";
         altText = "Image unknown or image is missing";
@@ -43,12 +42,19 @@ export async function createFeaturedPosts() {
       let timeStamp = lastUpdated;
       let updated = timeStamp.slice(0, 10);
 
-      futuresArticles.innerHTML += `
+      let postCategory;
+      if (post.category === null) {
+        postCategory = "unknown";
+      } else {
+        postCategory = post.category.title;
+      }
+
+      postContainer.innerHTML += `
       <div class="article__body">
       <div class="article__img" style="background-image: url('${imageURL}');">
       <button class="favorite_button" data-id="${postId}" data-title="${postTitle}" data-img="${imageURL}">Save as Favorite</button>
+      <p class="category__placement">${postCategory}</p>
       </div>
-      
       <h2 class="article__title">${postTitle}</h2>
       <p class="article__text">${post.summary}</p>
       <a href="/singlePost.html?id=${postId}">Read More</a>
@@ -58,16 +64,15 @@ export async function createFeaturedPosts() {
       </div>
     </div>`;
 
-    const favoritesButton = document.querySelectorAll(".favorite_button");
-    
-      favoritesButton.forEach(button => {
+      const favoritesButton = document.querySelectorAll(".favorite_button");
+
+      favoritesButton.forEach((button) => {
         currentFavPosts.forEach((post) => {
-          if(post.id === button.dataset.id){
-            button.classList.add("toggle")
+          if (post.id === button.dataset.id) {
+            button.classList.add("toggle");
           }
-        })
+        });
       });
-    
 
       favoritesButton.forEach((button) => {
         button.addEventListener("click", () => {
--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-import { createFeaturedPosts } from "./component/createHTML.js";
+import createFeaturedPosts from "./component/createHTML.js";
+import displayPosts from "./admin/dashboard.js";
 import { dropdown } from "./component/dropdownMenu.js";
-
-createFeaturedPosts()
+import { baseUrl } from "./data/api.js";
+import searchForPost from "./utils/searchPost.js";
 
 dropdown();
-   
+
+async function getPosts() {
+  try {
+    const featuredPostUrl = baseUrl + "articles";
+    const response = await fetch(featuredPostUrl);
+    const posts = await response.json();
+    
+    const { pathname } = document.location;
+    
+    console.log("test")
+    if (pathname === "/loginDashboard.html") {
+      displayPosts(posts);
+    } else {
+      createFeaturedPosts(posts);
+
+      searchForPost(posts);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getPosts();
