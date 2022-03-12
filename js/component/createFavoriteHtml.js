15b570b56e134b0832bad7c6e78833004d8e8b31
Josefine Holth <Josefine.Holth@hotmail.com>
2022-03-12 14:29:13 +0100

    fix the loader

--- js/component/createFavoriteHtml.js
+++ js/component/createFavoriteHtml.js
@@ -10,12 +10,17 @@ export function viewFavorites() {
   currentFavPosts = getFromStorage("favorite_article");
   container = document.querySelector(".post__container");
 
+  const loader = document.querySelector(".loader");
+  console.log(loader)
+  loader.style.display = "none";
+
   if (currentFavPosts.length === 0) {
     console.log("There is noting her");
     container.innerHTML = `<h3 style="line-height:2em">You don't have any posts stored just yet. So way dont you look <a href="/index.html">her</a> and find something.</h3>`;
   }
 
   currentFavPosts.forEach((post) => {
+
     container.innerHTML += `
     <div class="article__body">
     <div class="article__img" style="background-image: url('${post.img}');">
