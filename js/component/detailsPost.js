7f449ef66f1701667d68e24d570f8e2a3b6b2d92
Josefine Holth <Josefine.Holth@hotmail.com>
2022-03-12 14:29:28 +0100

    did some error fix

--- js/component/detailsPost.js
+++ js/component/detailsPost.js
@@ -6,15 +6,14 @@ dropdown();
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-
 if (!id) {
   document.location.href = "/";
 }
+const container = document.querySelector(".main__post");
 
 try {
-  const container = document.querySelector(".main__post");
-  const featuredPostUrl = baseUrl + "articles/"+ id;
-  
+  const featuredPostUrl = baseUrl + "articles/" + id;
+
   const response = await fetch(featuredPostUrl);
   const post = await response.json();
   const author = post.author;
@@ -22,25 +21,30 @@ try {
   const lastUpdated = post.updated_at;
   const title = post.title;
   const categories = post.category;
-  const coverImage = post.cover[0];
-  
+
   let altText;
 
   let timeStamp = lastUpdated;
   let updated = timeStamp.slice(0, 10);
+  let coverImage = post.cover[0];
 
-  if(!author || !categories || !body || !title){
-
+  if (!author || !categories || !body || !title) {
     container.innerHTML = `<div class="content__notFound">
     <p>No post was found</p>
     <a href="/">Back</a>
     </div>`;
   } else {
-
-    if(coverImage.alternativeText.length === 0){
-      altText = "No image or Image text was found";
-    }else{
-      altText = coverImage.alternativeText;
+    if (!coverImage) {
+      coverImage = {
+        alternativeText: "There is noe cover image her",
+        url: "/assets/img/image_note_found.png",
+      };
+    } else {
+      if (!coverImage.alternativeText) {
+        altText = "No image or Image text was found";
+      } else {
+        altText = coverImage.alternativeText;
+      }
     }
 
     container.innerHTML = `
@@ -53,8 +57,6 @@ try {
     </div>
     </div>`;
   }
-  
-  
 } catch (error) {
   container.innerHTML = error;
 }
